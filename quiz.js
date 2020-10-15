4112b55e58fd66f4e36c7c9e3fba2986db3c23fa
Marcel <googlillo@gmail.com>
2020-10-15 20:14:49 +0200

    Testing nextQuestion function

--- quiz.js
+++ quiz.js
@@ -6,8 +6,25 @@ function startQuiz() {
     console.log('Started')
     startButton.classList.add('hide')
     questionContainerElement.classList.remove('hide')
+    nextQuestion()
 }
 function nextQuestion() {
 }
 function selectAnswer() {
-} 
+}
+
+const questions = [
+    {
+        question: '',
+        amswers: [
+            {text: '', correct: true},
+            {text: '', correct: false},
+            {text: '', correct: false},
+            {text: '', correct: false},
+        ]
+    }
+]
+
+
+
+
